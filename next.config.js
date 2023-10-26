945ac908f2f588ad247951ea17b9b176892b8d0e
featherstoned <dontbotherme808@gmail.com>
2023-10-25 15:13:10 -1000

    first commit - through part 3 of tutorial

--- next.config.js
+++ next.config.js
@@ -1,6 +1,42 @@
+// ! from tutorial - error messages
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: "http",
+//         hostname: "localhost",
+//         port: "1337",
+//         pathname: "/uploads/**",
+//       },
+//     ],
+//   },
+// };
+
+// module.exports = nextConfig;
+
+//? from bard - this works 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "1337",
+        pathname: "/uploads/**",
+      },
+      {
+        protocol: "http",
+        hostname: "127.0.0.1",
+        port: "1337",
+        pathname: "/uploads/**",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
+
